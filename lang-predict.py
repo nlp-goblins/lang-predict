2379e864a71d6d686a4b6029aac8b0e0f9eecdc3
Michael P. Moran <mmutexas@gmail.com>
2019-05-09 09:40:10 -0500

    add notebook template

---
+++ lang-predict.py
@@ -0,0 +1,104 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.4'
+#       jupytext_version: 1.1.1
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# # Title
+# By
+
+# ## TODO
+#
+# - [ ] Acquisition
+#     - [ ] Select what list of repos to scrape.
+#     - [ ] Get requests form the site.
+#     - [ ] Save responses to csv.
+# - [ ] Preparation
+#     - [ ] Prepare the data for analysis.
+# - [ ] Exploration
+#     - [ ] Answer the following prompts:
+#         - [ ] What are the most common words in READMEs?
+#         - [ ] What does the distribution of IDFs look like for the most common words?
+#         - [ ] Does the length of the README vary by language?
+#         - [ ] Do different languages use a different number of unique words?
+# - [ ] Modeling
+#     - [ ] Transform the data for machine learning; use language to predict.
+#     - [ ] Fit several models using different text repressentations.
+#     - [ ] Build a function that will take in the text of a README file, and makes a prediction of language.
+# - [ ] Delivery
+#     - [ ] Github repo
+#         - [x] This notebook.
+#         - [ ] Documentation within the notebook.
+#         - [ ] README file in the repo.
+#         - [ ] Python scripts if applicable.
+#     - [ ] Google Slides
+#         - [ ] 1-2 slides only summarizing analysis.
+#         - [ ] Visualizations are labeled.
+#         - [ ] Geared for the general audience.
+#         - [ ] Share link @ readme file and/or classroom.
+
+# ## Table of contents
+# 1. [Project Planning](#project-planning)
+# 1. [Acquisition](#acquisition)
+# 1. [Preparation](#preparation)
+# 1. [Exploration](#exploration)
+# 1. [Modeling](#modeling)
+
+# ## Project Planning <a name="project-planning"></a>
+
+# ### Goals
+
+# ### Deliverables
+
+# ### Data Dictionary & Domain Knowledge
+
+# ### Hypotheses
+
+# ### Thoughts & Questions
+
+# ### Prepare the Environment
+
+# **Reload modules to capture changes**
+
+# ## Acquisition <a name="acquisition"></a>
+
+# ## Preparation <a name="preparation"></a>
+
+# ### Summarize Data
+
+# ### Handle Missing Values
+
+# ### Handle Duplicates
+
+# ### Fix Data Types
+
+# ### Handle Outliers
+
+# ### Check Missing Values
+
+# ## Exploration  <a name="exploration"></a>
+
+# ### Train-Test Split
+
+# ### Visualizations
+
+# ### Statistical Tests
+
+# ### Summarize Conclusions
+
+# ## Modeling <a name="modeling"></a>
+
+# ### Feature Engineering & Selection
+
+# ### Train & Test Models
+
+# ### Summarize Conclusions
